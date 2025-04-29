8bb3e842f5ec02f11ab6347400c2e97f4fde4ab0
Cralence <mahlerrrr76@gmail.com>
2025-04-29 15:01:28 +0800

    update

--- src/llamafactory/train/sft/__init__.py
+++ src/llamafactory/train/sft/__init__.py
@@ -14,5 +14,18 @@
 
 from .workflow import run_sft
 
+import torch
+import logging
 
-__all__ = ["run_sft"]
+orig_torch_load = torch.load
+
+def torch_wrapper(*args, **kwargs):
+    logging.warning("[comfyui-unsafe-torch] I have unsafely patched `torch.load`.  The `weights_only` option of `torch.load` is forcibly disabled.")
+    kwargs['weights_only'] = False
+
+    return orig_torch_load(*args, **kwargs)
+
+torch.load = torch_wrapper
+
+NODE_CLASS_MAPPINGS = {}
+__all__ = ['NODE_CLASS_MAPPINGS', "run_sft"]
